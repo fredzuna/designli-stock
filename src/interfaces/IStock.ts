e62463106d2b26392e99cc018c0d236214558bde
Fredy <fredzuna@hotmail.com>
2024-07-12 11:59:54 -0400

    create interfaces

---
+++ src/interfaces/IStock.ts
@@ -0,0 +1,7 @@
+export interface IStock {
+    symbol: string;
+    price: number;
+    change: number;
+    alertValue: number;
+  }
+  
---
+++ src/interfaces/IStockData.ts
@@ -0,0 +1,4 @@
+export interface IStockData {
+    name: string;
+    price: number;
+  }
